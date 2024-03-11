bb10db4b68e85aae4855d26e03fb0aa134488d45
Jan Zaba <j.zaba@tim.pl>
2024-03-11 21:46:52 +0100

    Add CartController test

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -6,11 +6,9 @@ use App\Model\Cart;
 use App\Model\CartItem;
 
 class CartController {
-    private Cart $cart;
 
-    public function __construct()
+    public function __construct(private readonly Cart $cart)
     {
-        $this->cart = new Cart();
     }
 
     public function addToCart($productID, $quantity): void
--- tests/Controller/CartControllerTest.php
+++ tests/Controller/CartControllerTest.php
@@ -3,9 +3,22 @@
 namespace App\Tests\Controller;
 
 use App\Controller\CartController;
+use App\Model\Cart;
+use App\Model\CartItem;
 use PHPUnit\Framework\TestCase;
 
 class CartControllerTest extends TestCase
 {
 
+    public function testAddToCart()
+    {
+        $cartMock = $this->createMock(Cart::class);
+        $cartController = new CartController($cartMock);
+        $cartMock
+            ->expects($this->once())
+            ->method('addItem')
+            ->with($this->isInstanceOf(CartItem::class));
+
+        $cartController->addToCart(1, 2);
+    }
 }
